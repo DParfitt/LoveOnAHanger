987851f16e759dfbe5e714b8e71316264e088150
Niek Saarberg <nsaarberg@xebia.com>
2022-09-21 18:01:13 +0200

    Upgraded dependencies

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
-const withTM = require("next-transpile-modules")(["@blockle/blocks-v2"]);
-const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withTM = require('next-transpile-modules')(['@blockle/blocks-v2']);
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin({
-  externals: ["@blockle/blocks-v2"],
+  externals: ['@blockle/blocks-v2'],
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config, { dev, isServer }) {
+    // Replace React with Preact only in client production build
+    // if (!dev && !isServer) {
+    //   Object.assign(config.resolve.alias, {
+    //     react: "preact/compat",
+    //     "react-dom": "preact/compat",
+    //   });
+    // }
+
+    return config;
+  },
 };
 
 module.exports = withVanillaExtract(withTM(nextConfig));
